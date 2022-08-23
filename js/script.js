799737862cc4a6314c5611ab9cac1ba96da9f5e2
Thales Alves <thalesalves758@gmail.com>
2022-08-23 16:52:34 -0300

    feat: create digital watch functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log("teste");
+const zeroPad = (num, places) => String(num).padStart(places, "0");
+
+const updateWatch = () => {
+  const currentTime = new Date();
+
+  document.querySelector(".hours").innerHTML = zeroPad(
+    currentTime.getHours(),
+    2
+  );
+
+  document.querySelector(".minutes").innerHTML = zeroPad(
+    currentTime.getMinutes(),
+    2
+  );
+
+  document.querySelector(".seconds").innerHTML = zeroPad(
+    currentTime.getSeconds(),
+    2
+  );
+};
+
+const initWatch = () => {
+  const ONE_SECOND = 1 * 1000;
+
+  updateWatch();
+
+  setInterval(updateWatch, ONE_SECOND);
+};
+
+initWatch();
